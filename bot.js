60dc28f5e06aa8640b1293867f9798ce46decc14
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-25 14:50:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,7 +227,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Vegeta Bot For Music`,"http://twitch.tv/S-F")
+client.user.setGame(`عمكم ليزر رجع يولاد المتناكة`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
